16772059d89699a49387f0ffaa63de19c5604c42
sonpasavee975 <sunpasavee@gmail.com>
2026-01-11 15:18:03 +0700

    lab04_commited

--- apps/lab04-photo-vue/src/composables/usePhotoGallery.ts
+++ apps/lab04-photo-vue/src/composables/usePhotoGallery.ts
@@ -1,9 +1,10 @@
-// CHANGE: Update import
 import { ref, watch, onMounted } from 'vue';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import type { Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
 
 export const usePhotoGallery = () => {
   const photos = ref<UserPhoto[]>([]);
@@ -19,16 +20,26 @@ export const usePhotoGallery = () => {
     });
 
     const fileName = Date.now() + '.jpeg';
+    // Save the picture and add it to photo collection
     const savedImageFile = await savePicture(capturedPhoto, fileName);
 
     photos.value = [savedImageFile, ...photos.value];
   };
 
   const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-    // Fetch the photo, read as a blob, then convert to base64 format
-    const response = await fetch(photo.webPath!);
-    const blob = await response.blob();
-    const base64Data = (await convertBlobToBase64(blob)) as string;
+    let base64Data: string | Blob;
+    // "hybrid" will detect mobile - iOS or Android
+    if (isPlatform('hybrid')) {
+      const readFile = await Filesystem.readFile({
+        path: photo.path!,
+      });
+      base64Data = readFile.data;
+    } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      base64Data = (await convertBlobToBase64(blob)) as string;
+    }
 
     const savedFile = await Filesystem.writeFile({
       path: fileName,
@@ -36,12 +47,20 @@ export const usePhotoGallery = () => {
       directory: Directory.Data,
     });
 
-    // Use webPath to display the new image instead of base64 since it's
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: photo.webPath,
-    };
+    if (isPlatform('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
   };
 
   const convertBlobToBase64 = (blob: Blob) => {
@@ -66,18 +85,21 @@ export const usePhotoGallery = () => {
     const photoList = await Preferences.get({ key: PHOTO_STORAGE });
     const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
 
-    for (const photo of photosInPreferences) {
-      const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: Directory.Data,
-      });
-      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    // If running on the web...
+    if (!isPlatform('hybrid')) {
+      for (const photo of photosInPreferences) {
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
     }
 
     photos.value = photosInPreferences;
   };
 
-  // CHANGE: Add call to `onMounted()` with the `loadSaved()` method
   onMounted(loadSaved);
   watch(photos, cachePhotos);
 
